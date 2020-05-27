97fe395c08320ac7ff3045679ea6159244f5ac9c
networkop <mmkashin@gmail.com>
2020-05-27 18:27:51 +0100

    Added unit tests and IP/host check on service LoadBalancer
    Signed-off-by: networkop <mmkashin@gmail.com>

--- plugin/kubernetes/object/service.go
+++ plugin/kubernetes/object/service.go
@@ -62,8 +62,12 @@ func toService(skipCleanup bool, svc *api.Service) *Service {
 
 	li := copy(s.ExternalIPs, svc.Spec.ExternalIPs)
 	for i, lb := range svc.Status.LoadBalancer.Ingress {
-		s.ExternalIPs[li+i] = lb.IP
-		s.ExternalIPs[li+i] = lb.Hostname
+		if &lb.IP != nil {
+			s.ExternalIPs[li+i] = lb.IP
+		} else if &lb.Hostname != nil {
+			s.ExternalIPs[li+i] = lb.Hostname
+		}
+
 	}
 
 	if !skipCleanup {
